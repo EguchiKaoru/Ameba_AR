12a5b9e1aefdb3e2b0aa63e9b31293fa08cfbc7b
*ip8REnlTfQ <kishie@takeuchi-const.co.jp>
2025-03-28 16:07:48 +0900

    for log test

--- Assets/Scripts/ARMarkerDetector.cs
+++ Assets/Scripts/ARMarkerDetector.cs
@@ -27,6 +27,7 @@ public class ARMarkerDetector : MonoBehaviour
     {
         foreach (var trackedImage in eventArgs.added)
         {
+            Debug.Log($"Tracked image added: {trackedImage.referenceImage.name}");
             var modelInstance = Instantiate(modelPrefab, trackedImage.transform.position, trackedImage.transform.rotation);
             var modelController = modelInstance.GetComponent<ModelController>();
             if (modelController != null)
@@ -37,11 +38,12 @@ public class ARMarkerDetector : MonoBehaviour
 
         foreach (var trackedImage in eventArgs.updated)
         {
+            Debug.Log($"Tracked image updated: {trackedImage.referenceImage.name}");
             // 必要に応じて位置・回転の更新など
         }
-
         foreach (var trackedImage in eventArgs.removed)
         {
+            Debug.Log($"Tracked image removed: {trackedImage.referenceImage.name}");
             // マーカーが見えなくなった際の処理（オブジェクト削除など）
         }
     }
