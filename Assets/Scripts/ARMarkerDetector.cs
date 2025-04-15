e57f1823c2b3ec4927d724dc7b796b995ee67cd5
*ip8REnlTfQ <kishie@takeuchi-const.co.jp>
2025-04-15 11:34:22 +0900

    fixed arM-Detector

--- Assets/Scripts/ARMarkerDetector.cs
+++ Assets/Scripts/ARMarkerDetector.cs
@@ -3,13 +3,17 @@ using UnityEngine.XR.ARFoundation;
 using UnityEngine.XR.ARSubsystems;
 
 /// <summary>
-/// ARTrackedImageManagerのイベントを受け取り、対応するPrefabを生成するサンプル
+/// ARTrackedImageManager のイベントを受け取り、対応する Prefab を生成するサンプル
+/// (生成時にモデルをトラッキング対象の子から切り離す)
 /// </summary>
 public class ARMarkerDetector : MonoBehaviour
 {
     [SerializeField] private ARTrackedImageManager trackedImageManager;
     [SerializeField] private GameObject modelPrefab;
     [SerializeField] private CommandInvoker invoker;
+
+    // 【オプション】モデルを別のコンテナに入れたい場合は、下記のコメントを外して使ってください
+    // [SerializeField] private Transform modelsContainer;
 
     private void OnEnable()
     {
@@ -23,57 +27,41 @@ public class ARMarkerDetector : MonoBehaviour
             trackedImageManager.trackedImagesChanged -= OnTrackedImagesChanged;
     }
 
-    // private void OnTrackedImagesChanged(ARTrackedImagesChangedEventArgs eventArgs)
-    // {
-    //     foreach (var trackedImage in eventArgs.added)
-    //     {
-    //         Debug.Log($"Tracked image added: {trackedImage.referenceImage.name}");
-    //         var modelInstance = Instantiate(modelPrefab, trackedImage.transform.position, trackedImage.transform.rotation);
-    //         var modelController = modelInstance.GetComponent<ModelController>();
-    //         if (modelController != null)
-    //         {
-    //             modelController.invoker = invoker;
-    //         }
-    //     }
-
-    //     foreach (var trackedImage in eventArgs.updated)
-    //     {
-    //         Debug.Log($"Tracked image updated: {trackedImage.referenceImage.name}");
-    //         // 必要に応じて位置・回転の更新など
-    //     }
-    //     foreach (var trackedImage in eventArgs.removed)
-    //     {
-    //         Debug.Log($"Tracked image removed: {trackedImage.referenceImage.name}");
-    //         // マーカーが見えなくなった際の処理（オブジェクト削除など）
-    //     }
-    // }
-
     private void OnTrackedImagesChanged(ARTrackedImagesChangedEventArgs eventArgs)
     {
+        // マーカーが新たに検出されたとき
         foreach (var trackedImage in eventArgs.added)
         {
             Debug.Log($"Tracked image added: {trackedImage.referenceImage.name}");
-            var modelInstance = Instantiate(modelPrefab, trackedImage.transform.position, trackedImage.transform.rotation);
-            var modelController = modelInstance.GetComponent<ModelController>();
+
+            // Instantiate 時に親を指定しない（または、必要なら別のコンテナに配置する）
+            GameObject modelInstance = Instantiate(modelPrefab, trackedImage.transform.position, trackedImage.transform.rotation);
+            // 【オプション】モデルを特定のコンテナに配置する場合
+            // GameObject modelInstance = Instantiate(modelPrefab, trackedImage.transform.position, trackedImage.transform.rotation, modelsContainer);
+
+            // ModelController コンポーネントがあればシーン上の CommandInvoker を注入
+            ModelController modelController = modelInstance.GetComponent<ModelController>();
             if (modelController != null)
             {
                 modelController.invoker = invoker;
             }
         }
 
+        // マーカーの更新（Tracking 状態の変化に応じて必要な処理を追加）
         foreach (var trackedImage in eventArgs.updated)
         {
-            // 更新が必要な場合にのみログ出力する例
             if (trackedImage.trackingState == TrackingState.Tracking)
             {
                 Debug.Log($"Tracked image is tracking: {trackedImage.referenceImage.name}");
             }
+            // ここで、ユーザー操作のオフセットを適用する等の処理を追加することも検討できる
         }
 
+        // マーカーが失われたとき
         foreach (var trackedImage in eventArgs.removed)
         {
             Debug.Log($"Tracked image removed: {trackedImage.referenceImage.name}");
+            // ここで、生成済みのモデルの削除や非表示にする処理を追加してください
         }
     }
-
 }
